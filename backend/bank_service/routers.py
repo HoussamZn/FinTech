923a314977270be0485cb6ca70bb038aca0d5308
HoussamZn <houssamzitan2003@gmail.com>
2025-04-08 17:55:25 +0100

    transactions

--- backend/bank_service/routers.py
+++ backend/bank_service/routers.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,HTTPException
 from fastapi import Depends
 from sqlalchemy.orm import Session
 from models import BankAccount,BankAccountCreate
 from database import get_db
+from services import get_bank_by_account_number,create_bank
 
 
 router = APIRouter()
@@ -16,6 +17,17 @@ BACKENDS = {
 @router.get("/")
 def home():
     return {"message": "Service Bank responding"}
+
+@router.get("/acc/{number}")
+def get_acc(number:str,db: Session = Depends(get_db)):
+    acc = get_bank_by_account_number(db=db,account_number=number)
+    if acc is None:
+        raise HTTPException(status_code=404, detail="No account with this number")
+    return acc
+
+@router.post("/acc")
+def create_acc(acc:BankAccountCreate,db: Session = Depends(get_db)):
+    return create_bank(db=db,acc=acc)
 
 
 
--- backend/bank_service/services.py
+++ backend/bank_service/services.py
@@ -1,8 +1,16 @@
 from sqlalchemy.orm import Session
-from models import BankAccount
+from models import BankAccount,BankAccountCreate
 
 def get_bank_by_account_number(db: Session, account_number: str):
     return db.query(BankAccount).filter(BankAccount.account_number == account_number).first()
+
+def create_bank(db: Session, acc: BankAccountCreate):
+    bank_account_dict = acc.model_dump()
+    db_acc = BankAccount(**bank_account_dict)
+    db.add(db_acc)
+    db.commit()
+    db.refresh(db_acc)
+    return {"message": "Bank account created successfully!"}
 
 def check_balance(amount: float,bank_account:BankAccount):
     if bank_account.balance >= amount : return True
